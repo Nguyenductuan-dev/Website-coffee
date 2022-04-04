744aa443e6bb7776a0e7d2a435d3795422725ca2
Nguyenductuan-dev <ndtuan96nb@gmail.com>
2022-04-04 22:26:08 +0700

    Website coffee

---
+++ .history/assets/js/main_20220403154310.js
@@ -0,0 +1,12 @@
+
+
+/* =====================SHOW MENU========================= */
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
+
+if(navToggle) {
+    navToggle.addEventListener('click' () => {
+        navMenu.classList.add('show-menu');
+    })
+}
---
+++ .history/assets/js/main_20220403155324.js
@@ -0,0 +1,14 @@
+
+
+/* ===================== SHOW MENU ========================= */
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
+
+if(navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+    })
+}
+
+/* ===================== REMOVE MENU MOBILE ========================= */
